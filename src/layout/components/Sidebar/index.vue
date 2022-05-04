cbf47d46b505a42becec3fbb9b296056adeda81e
zhangke <1826213722@qq.com>
2022-05-04 22:16:02 +0800

    feat: 创建页面组件，使用临时 menu 菜单

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="">Sidebar</div>
+  <div class="sidebar-container">
+    <h1>占位</h1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import SidebarMenu from './SidebarMenu'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/styles/sidebar.scss';
+</style>
