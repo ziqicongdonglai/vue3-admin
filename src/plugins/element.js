bc49cb33a9458d1df5cf022456ef523954585464
zhangke <1826213722@qq.com>
2022-05-04 23:58:53 +0800

    feat: element-plus 国际化处理

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,11 @@
+import store from '@/store'
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en'
 
 export default (app) => {
-  app.use(ElementPlus, { locale })
+  app.use(ElementPlus, {
+    locale: store.getters.language === 'en' ? en : zhCn
+  })
 }
