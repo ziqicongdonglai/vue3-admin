830cb540fcd886a145dbc1224429be7910f3475f
zhangke <1826213722@qq.com>
2022-05-04 23:12:52 +0800

    feat: 渲染基本的面包屑组件

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navbar">
     <hamburger class="hamburger-container" />
+    <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
@@ -26,6 +27,7 @@
 
 <script setup>
 import Hamburger from '@/components/Hamburger'
+import Breadcrumb from '@/components/Breadcrumb'
 import {} from 'vue'
 import { useStore } from 'vuex'
 
@@ -55,6 +57,10 @@ const logout = () => {
     &:hover {
       background: rgba(0, 0, 0, 0.1);
     }
+  }
+
+  .breadcrumb-container {
+    float: left;
   }
 
   .right-menu {
