05ef64b7261e72ad07aaef632b18a01a6f19829b
zhangke <1826213722@qq.com>
2022-05-04 19:25:32 +0800

    ESLint 与 Prettier 配合解决代码格式问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'warn',
+    'eslint-disable-next-line': 'off',
+    'no-undef': 'off'
   }
 }
