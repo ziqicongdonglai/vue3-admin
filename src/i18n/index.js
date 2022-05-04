968c00d71640bb57677782c9fd7c6b88c94e3897
zhangke <1826213722@qq.com>
2022-05-05 00:39:15 +0800

    feat: 国际化缓存处理

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import mZhLocale from './lang/zh'
 import mEnLocale from './lang/en'
+import store from '@/store'
 
 // 创建 message 数据源
 const messages = {
@@ -19,13 +20,20 @@ const messages = {
 // 创建 locale 语言变量
 const locale = 'zh'
 
+/**
+ * 返回当前 lang
+ */
+function getLanguage() {
+  return store && store.getters && store.getters.language
+}
+
 // 初始化 i18n 实例
 const i18n = createI18n({
   // 使用 Composition API 模式，则需要将其设置为false
   legacy: false,
   // 全局注入 $t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
