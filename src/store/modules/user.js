1cc990d4574306277d173feaf25fba8c86169ca5
zhangke <1826213722@qq.com>
2022-05-04 20:34:03 +0800

    feat: 封装请求动作

---
+++ src/store/modules/user.js
@@ -0,0 +1,25 @@
+import { login } from '@/api/sys'
+import md5 from 'md5'
+export default {
+  namespaced: true,
+  state: () => ({}),
+  mutations: {},
+  actions: {
+    login(context, userInfo) {
+      const { username, password } = userInfo
+      console.log(md5(password))
+      return new Promise((resolve, reject) => {
+        login({
+          username,
+          password: md5(password)
+        })
+          .then((data) => {
+            resolve(data)
+          })
+          .catch((err) => {
+            reject(err)
+          })
+      })
+    }
+  }
+}
