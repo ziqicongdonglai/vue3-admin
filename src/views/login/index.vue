266596de5ec234be49d5e0caf55ca28929e7bbdd
zhangke <1826213722@qq.com>
2022-05-04 20:56:38 +0800

    feat: 登录后的处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -33,6 +33,7 @@
 import { ref } from 'vue'
 import { validatePassword } from './rules'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 
 // 数据源
 const loginForm = ref({
@@ -71,6 +72,7 @@ const onChangePwdType = () => {
 const loading = ref(false)
 const loginFormRef = ref(null)
 const store = useStore()
+const router = useRouter()
 const handleLogin = () => {
   loginFormRef.value.validate((valid) => {
     if (!valid) return
@@ -81,7 +83,7 @@ const handleLogin = () => {
       .then((res) => {
         loading.value = false
         // TODO: 登录后操作
-        console.log(res)
+        router.push('/')
       })
       .catch((err) => {
         console.log(err)
