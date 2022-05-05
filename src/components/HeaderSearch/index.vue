78ea63d995ecfac15c60898b88c249e24b441666
zhangke <1826213722@qq.com>
2022-05-05 21:18:34 +0800

    feat: 生成 searchPool，渲染检索数据

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -12,16 +12,23 @@
       :remote-method="querySearch"
       @change="onSelectChange"
     >
-      <el-option v-for="option in 5" :key="option" :label="option" :value="option"></el-option>
+      <!-- <el-option v-for="option in 5" :key="option" :label="option" :value="option"></el-option> -->
+      <el-option
+        v-for="option in searchOptions"
+        :key="option.item.path"
+        :label="option.item.title.join(' > ')"
+        :value="option.item"
+      ></el-option>
     </el-select>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-import { filterRouters, generateMenus } from '@/utils/route'
-import Fuse from 'fuse.js'
+import { ref, computed, watch } from 'vue'
+import { filterRouters } from '@/utils/route'
 import { useRouter } from 'vue-router'
+import { generateRoutes } from './FuseData'
+import Fuse from 'fuse.js'
 
 // 控制 search 显示
 const isShow = ref(false)
@@ -31,31 +38,37 @@ const onShowClick = () => {
   isShow.value = !isShow.value
   headerSearchSelectRef.value.focus()
 }
+// 搜索结果
+const searchOptions = ref([])
 
 // search 相关
 const search = ref('')
 // 搜索方法
-const querySearch = () => {
-  console.log('querySearch')
+const querySearch = (query) => {
+  // console.log(fuse.search(query))
+  if (query !== '') {
+    searchOptions.value = fuse.search(query)
+  } else {
+    searchOptions.value = []
+  }
 }
 // 选中回调
-const onSelectChange = () => {
-  console.log('onSelectChange')
+const onSelectChange = (val) => {
+  router.push(val.path)
+  onClose()
 }
 
 // 检索数据源
 const router = useRouter()
 const searchPool = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
-  console.log(generateMenus(filterRoutes))
-  return generateMenus(filterRoutes)
+  return generateRoutes(filterRoutes)
 })
-console.log(searchPool)
 
 /**
  * 搜索库相关
  */
-const fuse = new Fuse(list, {
+const fuse = new Fuse(searchPool.value, {
   // 是否按优先级进行排序
   shouldSort: true,
   // 匹配长度超过这个值的才会被认为是匹配的
@@ -74,6 +87,7 @@ const fuse = new Fuse(list, {
     }
   ]
 })
+console.log(searchPool)
 </script>
 
 <style lang="scss" scoped>
