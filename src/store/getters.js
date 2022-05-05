06ea6c676a49b1fb6d2bb4da071c7bc2150040bb
zhangke <1826213722@qq.com>
2022-05-05 13:56:19 +0800

    feat: 创建 SelectColor 组件

--- src/store/getters.js
+++ src/store/getters.js
@@ -10,6 +10,7 @@ const getters = {
   },
   cssVar: () => variables,
   sidebarOpened: (state) => state.app.sidebarOpened,
-  language: (state) => state.app.language
+  language: (state) => state.app.language,
+  mainColor: (state) => state.theme.mainColor
 }
 export default getters
