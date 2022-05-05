e169664eb487413377cd9443d3c83957458f1a77
zhangke <1826213722@qq.com>
2022-05-05 21:09:45 +0800

    feat: 检索数据源，路由表数据处理

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -18,7 +18,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { filterRouters, generateMenus } from '@/utils/route'
+import Fuse from 'fuse.js'
+import { useRouter } from 'vue-router'
 
 // 控制 search 显示
 const isShow = ref(false)
@@ -39,6 +42,38 @@ const querySearch = () => {
 const onSelectChange = () => {
   console.log('onSelectChange')
 }
+
+// 检索数据源
+const router = useRouter()
+const searchPool = computed(() => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  console.log(generateMenus(filterRoutes))
+  return generateMenus(filterRoutes)
+})
+console.log(searchPool)
+
+/**
+ * 搜索库相关
+ */
+const fuse = new Fuse(list, {
+  // 是否按优先级进行排序
+  shouldSort: true,
+  // 匹配长度超过这个值的才会被认为是匹配的
+  minMatchCharLength: 1,
+  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+  // name：搜索的键
+  // weight：对应的权重
+  keys: [
+    {
+      name: 'title',
+      weight: 0.7
+    },
+    {
+      name: 'path',
+      weight: 0.3
+    }
+  ]
+})
 </script>
 
 <style lang="scss" scoped>
