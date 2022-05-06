472a45e6c778d182b6847ed92416cc8588dac629
zhangke <1826213722@qq.com>
2022-05-07 00:25:21 +0800

    feat: 创建 MyAvatar 头像组件

---
+++ src/views/profile/components/ProjectCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    <my-avatar :image="$store.getters.userInfo.avatar" :height="'100px'" :width="'100px'" :hoverable="false">
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </my-avatar>
+  </div>
+</template>
+
+<script setup>
+import MyAvatar from '@/components/MyAvatar'
+</script>
+
+<style lang="scss" scoped></style>
