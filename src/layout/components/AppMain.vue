23e98f58e427db0e2007939f52c7543217985dfa
zhangke <1826213722@qq.com>
2022-05-06 23:10:53 +0800

    feat: 创建 tags 数据源

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -5,7 +5,52 @@
 </template>
 
 <script setup>
-import {} from 'vue'
+import { watch } from 'vue'
+import { isTags } from '@/utils/tags'
+import { generateTitle } from '@/utils/i18n'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+
+const route = useRoute()
+
+/**
+ * 生成 title
+ */
+const getTitle = (route) => {
+  let title = ''
+  if (!route.meta) {
+    // 处理无 meta 的路由
+    const pathArr = route.path.split('/')
+    title = pathArr[pathArr.length - 1]
+  } else {
+    title = generateTitle(route.meta.title)
+  }
+  return title
+}
+
+/**
+ * 监听路由变化
+ */
+const store = useStore()
+watch(
+  route,
+  (to, from) => {
+    if (!isTags(to.path)) return
+    const { fullPath, meta, name, params, path, query } = to
+    store.commit('app/addTagsViewList', {
+      fullPath,
+      meta,
+      name,
+      params,
+      path,
+      query,
+      title: getTitle(to)
+    })
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
 <style lang="scss" scoped>
