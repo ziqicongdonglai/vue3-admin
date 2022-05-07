6d8cfb556475b050709984c979e2d09df8dfac32
zhangke <1826213722@qq.com>
2022-05-07 19:47:48 +0800

    feat: 章节模块开发

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,19 @@
 import request from '@/utils/request'
 
+/**
+ * 获取功能
+ */
 export const getFeature = () => {
   return request({
     url: '/user/feature'
+  })
+}
+
+/**
+ * 获取章节
+ */
+export const getChapter = () => {
+  return request({
+    url: '/user/chapter'
   })
 }
---
+++ src/views/profile/components/Chapter.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-timeline>
+    <el-timeline-item v-for="item in chapterData" :key="item.id" :timestamp="item.timestamp" placement="top">
+      <el-card>
+        <h4>{{ item.content }}</h4>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
+</template>
+<script setup>
+import { watchSwitchLang } from '@/utils/i18n'
+import { getChapter } from '@/api/user'
+import { ref } from 'vue'
+const chapterData = ref([])
+
+const getChapterData = async () => {
+  chapterData.value = await getChapter()
+}
+getChapterData()
+
+// 监听语言切换
+watchSwitchLang(getChapterData)
+</script>
