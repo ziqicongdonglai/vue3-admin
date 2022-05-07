589161eac68541a22f1725d985efa470c54f746a
zhangke <1826213722@qq.com>
2022-05-07 22:07:08 +0800

    feat: 传递解析后的 excel 数据

--- src/api/user-manage.js
+++ src/api/user-manage.js
@@ -9,3 +9,14 @@ export const getUserManageList = (data) => {
     params: data
   })
 }
+
+/**
+ * 批量导入
+ */
+export const userBatchImport = (data) => {
+  return request({
+    url: '/user-manage/batch/import',
+    method: 'POST',
+    data
+  })
+}
