1ac42774bd9909cc15b72c0ca68742ab98bca12d
zhangke <1826213722@qq.com>
2022-05-07 23:31:56 +0800

    feat: 获取展示数据

--- src/views/user-info/index.vue
+++ src/views/user-info/index.vue
@@ -3,7 +3,26 @@
 </template>
 
 <script setup>
-import {} from 'vue'
+import { userDetail } from '@/api/user-manage'
+import { watchSwitchLang } from '@/utils/i18n'
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  }
+})
+
+// 数据相关
+const detailData = ref({})
+const getUserDetail = async () => {
+  detailData.value = await userDetail(props.id)
+}
+getUserDetail()
+
+// 语言切换
+watchSwitchLang(getUserDetail)
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -34,8 +34,10 @@
           </template>
         </el-table-column>
         <el-table-column :label="$t('msg.excel.action')" fixed="right" width="260">
-          <template #default>
-            <el-button type="primary" size="mini">{{ $t('msg.excel.show') }}</el-button>
+          <template #default="{ row }">
+            <el-button type="primary" size="mini" @click="onShowClick(row._id)">
+              {{ $t('msg.excel.show') }}
+            </el-button>
             <el-button type="info" size="mini">{{ $t('msg.excel.showRole') }}</el-button>
             <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{ $t('msg.excel.remove') }}</el-button>
           </template>
@@ -131,6 +133,13 @@ const onRemoveClick = (row) => {
 const exportToExcelVisible = ref(false)
 const onToExcelClick = () => {
   exportToExcelVisible.value = true
+}
+
+/**
+ * 查看按钮点击事件
+ */
+const onShowClick = (id) => {
+  router.push(`/user/info/${id}`)
 }
 </script>
 
