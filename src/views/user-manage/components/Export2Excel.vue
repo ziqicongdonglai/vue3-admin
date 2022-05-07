5257e94fe75ca71dde0c42a0928c3d54089cccd2
zhangke <1826213722@qq.com>
2022-05-07 22:58:17 +0800

    feat: 导出前置业务处理

--- src/views/user-manage/components/Export2Excel.vue
+++ src/views/user-manage/components/Export2Excel.vue
@@ -1,16 +1,23 @@
 <template>
   <el-dialog :title="$t('msg.excel.title')" :model-value="modelValue" @close="closed" width="30%">
-    <el-input :placeholder="$t('msg.excel.placeholder')"></el-input>
+    <el-input v-model="excelName" :placeholder="$t('msg.excel.placeholder')"> </el-input>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="closed">{{ $t('msg.excel.close') }}</el-button>
-        <el-button type="primary" @click="onConfirm">{{ $t('msg.excel.confirm') }}</el-button>
+        <el-button type="primary" @click="onConfirm" :loading="loading">
+          {{ $t('msg.excel.confirm') }}
+        </el-button>
       </span>
     </template>
   </el-dialog>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { watchSwitchLang } from '@/utils/i18n'
+import { useI18n } from 'vue-i18n'
+import { getUserManageAllList } from '@/api/user-manage'
+
 defineProps({
   modelValue: {
     type: Boolean,
@@ -22,7 +29,11 @@ const emits = defineEmits(['update:modelValue'])
 /**
  * 导出按钮点击事件
  */
+const loading = ref(false)
 const onConfirm = async () => {
+  loading.value = true
+  const allUser = (await getUserManageAllList()).list
+  console.log(allUser)
   closed()
 }
 
@@ -30,6 +41,16 @@ const onConfirm = async () => {
  * 关闭
  */
 const closed = () => {
+  loading.value = false
   emits('update:modelValue', false)
 }
+
+const i18n = useI18n()
+let exportDefaultName = i18n.t('msg.excel.defaultName')
+const excelName = ref('')
+excelName.value = exportDefaultName
+watchSwitchLang(() => {
+  exportDefaultName = i18n.t('msg.excel.defaultName')
+  excelName.value = exportDefaultName
+})
 </script>
