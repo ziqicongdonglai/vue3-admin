f91254110c3f6dbc4dde5be9e13984e8b2634203
zhangke <1826213722@qq.com>
2022-05-09 20:54:29 +0800

    feat: 创建文章基本结构实现

--- src/views/article-create/index.vue
+++ src/views/article-create/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>article-create</div>
+  <div class="article-create">
+    <el-card>
+      <el-input
+        class="title-input"
+        :placeholder="$t('msg.article.titlePlaceholder')"
+        v-model="title"
+        maxlength="20"
+        clearable
+      >
+      </el-input>
+      <el-tabs v-model="activeName">
+        <el-tab-pane :label="$t('msg.article.markdown')" name="markdown">
+          <markdown></markdown>
+        </el-tab-pane>
+        <el-tab-pane :label="$t('msg.article.richText')" name="editor">
+          <editor></editor>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import Editor from './components/Editor.vue'
+import Markdown from './components/Markdown.vue'
+import { ref } from 'vue'
+
+const activeName = ref('markdown')
+const title = ref('')
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.title-input {
+  margin-bottom: 20px;
+}
+</style>
