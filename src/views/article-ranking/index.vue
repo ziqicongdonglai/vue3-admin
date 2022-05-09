599bb96b1a8119cccc93299219e8d527795d2568
zhangke <1826213722@qq.com>
2022-05-09 20:11:29 +0800

    feat: 实现动态表格能力

--- src/views/article-ranking/index.vue
+++ src/views/article-ranking/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="article-ranking-container">
+    <el-card class="header">
+      <div class="dynamic-box">
+        <span class="title">{{ $t('msg.article.dynamicTitle') }}</span>
+        <el-checkbox-group v-model="selectDynamicLabel">
+          <el-checkbox v-for="(item, index) in dynamicData" :label="item.label" :key="index"
+            >{{ item.label }}
+          </el-checkbox>
+        </el-checkbox-group>
+      </div>
+    </el-card>
+
     <el-card>
       <el-table ref="tableRef" :data="tableData" border>
-        <el-table-column :label="$t('msg.article.ranking')" prop="ranking"></el-table-column>
-        <el-table-column :label="$t('msg.article.title')" prop="title"></el-table-column>
-        <el-table-column :label="$t('msg.article.author')" prop="author"></el-table-column>
-        <el-table-column :label="$t('msg.article.publicDate')">
-          <template #default="{ row }">
+        <el-table-column v-for="(item, index) in tableColumns" :key="index" :prop="item.prop" :label="item.label">
+          <template #default="{ row }" v-if="item.prop === 'publicDate'">
             {{ $filters.relativeTime(row.publicDate) }}
           </template>
-        </el-table-column>
-
-        <el-table-column :label="$t('msg.article.desc')" prop="desc"></el-table-column>
-        <el-table-column :label="$t('msg.article.action')">
-          <el-button type="primary" size="mini" @click="onShowClick(row)">{{ $t('msg.article.show') }}</el-button>
-          <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{ $t('msg.article.remove') }}</el-button>
+          <template #default="{ row }" v-else-if="item.prop === 'action'">
+            <el-button type="primary" size="mini" @click="onShowClick(row)">{{ $t('msg.article.show') }}</el-button>
+            <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{ $t('msg.article.remove') }}</el-button>
+          </template>
         </el-table-column>
       </el-table>
 
@@ -37,6 +43,8 @@
 import { ref, onActivated } from 'vue'
 import { getArticleList } from '@/api/article'
 import { watchSwitchLang } from '@/utils/i18n'
+// import {  } from './dynamic'
+import { dynamicData, selectDynamicLabel, tableColumns } from './dynamic'
 /**
  * size 改变触发
  */
